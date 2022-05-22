e3ebe8ef19098fd3a43d6f9713a00c768a2978a6
HenryDelaossa <delaossah@gmail.com>
2022-05-22 16:46:22 -0500

    change svg from NoFavoritesPokemon

--- components/ui/NoFavoritsPokemon.tsx
+++ components/ui/NoFavoritsPokemon.tsx
@@ -12,7 +12,7 @@ export const NoFavorit = () => {
     }}>
         <Text>no hay favoritos</Text>
         <Image
-            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg'
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg'
             width={250}
             height={250}
             css={{
